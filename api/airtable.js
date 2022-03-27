d02d95eb3ce127522e8abe50dba5ed42825e5a70
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2022-03-27 17:45:04 -0400

    Cleanup

---
+++ api/airtable.js
@@ -0,0 +1,35 @@
+const Airtable = require("airtable");
+const xTokens = require("../utils").xTokens;
+
+const base = new Airtable({
+  apiKey: process.env.REACT_APP_AIRTABLE_API_KEY,
+}).base("appO3hFgzO9X7jbIQ");
+
+export const getSides = (symbol) => {
+  const table = "sides";
+
+  return new Promise((resolve, reject) => {
+    let votes = {};
+    base(`${table}`)
+      .select({
+        maxRecords: 100,
+        view: "Grid view",
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function (record) {
+            console.log("recipee: ", record.get("recipe"));
+          });
+
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(votes);
+          }
+        }
+      );
+  });
+};
